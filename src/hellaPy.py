05f243babafcd1e852771aa49dd5eeaa18c9bdf8
Nathaniel Hellabyte <nate@hellabit.es>
2015-11-19 08:13:26 -0700

    updates

--- src/hellaPy.py
+++ src/hellaPy.py
@@ -33,13 +33,21 @@ rcParams.update({
   'lines.linewidth'       : 1,
   'lines.linewidth'       : 1,
   # Fonts
-  'font.serif'         : [ 'Times New Roman',
-      'Latin Modern Roman','Computer Modern Roman','Bitstream Vera Serif'
+  'font.serif'         : [ 
+      'Times New Roman',
+      'Latin Modern Roman',
+      'Computer Modern Roman',
+      'Bitstream Vera Serif',
   ], 
   'font.sans-serif'    : [
-      'Open Sans','Computer Modern Sans','Bitstream Vera Sans'
+      'Open Sans',
+      'Computer Modern Sans',
+      'Bitstream Vera Sans'
   ],
-  'font.monospace'     : ['Anonymous Pro', 'Bitstream Vera Sans Mono'],
+  'font.monospace'     : [
+      'Anonymous Pro', 
+      'Bitstream Vera Sans Mono'
+  ],
 })
 
 class ColorBasis:
@@ -63,6 +71,7 @@ class ColorBasis:
 class hellaPy:
   def __init__(self,*args):
     self.fontfamily = 'serif'
+    self.usetex     = False
     for arg in args:
       if arg == 'sans':
         self.fontfamily = 'sans-serif'
@@ -78,6 +87,12 @@ class hellaPy:
     })
     return None
 
+  def set_tex(self,boolean=False):
+    self.usetex = boolean
+    rcParams.update({
+      'text.usetex' : self.usetex 
+    })
+# End Class
 
 # Colors
 yellow = numpy.array([1.0,1.0,0.0,0.8])
@@ -87,11 +102,26 @@ blue   = numpy.array([0.0,0.7,0.8,0.8])
 teal   = numpy.array([0.0,0.8,0.6,0.8])
 gray   = numpy.array([0.3,0.3,0.3,0.5])
 
-dyellow = numpy.array([0.6,0.6,0.0,0.6])
-dred    = numpy.array([0.6,0.0,0.0,0.6])
+dyellow = numpy.array([0.7,0.7,0.0,0.6])
+dred    = numpy.array([0.7,0.0,0.0,0.6])
 
 font_style = hellaPy('serif')
 
 # axes style with auto layout and no labels
 #re = [.1408,.128,.79,.8115]
 
+def basic_plotting_help():
+  print('                                                            \n\
+data_array   = loadtxt(datafilename)                                 \n\
+xdata, ydata = data_array[:,0], data_array[:,1]                      \n\
+                                                                     \n\
+figure(figsize=(l,w),dpi=r) # l,w are floats that represent inches   \n\
+clf()                                                                \n\
+plot(xdata,ydata,style)     # style default is "-b" (blue solid line)\n\
+xlabel(x_string)                                                     \n\
+ylabel(y_string)                                                     \n\
+legend(array_of_strings)    # Does not have to be an array           \n\
+title(title_string)                                                  \n\
+savefig("fig.eps")                                                   \n\
+  ')
+  return None
