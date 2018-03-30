5204b6e6592d2430fa3610719c6cb9d7283908ba
Nathaniel Hellabyte <nate@hellabit.es>
2018-03-30 01:38:48 -0700

    init

--- src/hellaPy.py
+++ src/hellaPy.py
@@ -298,8 +298,8 @@ except Exception as ex:
 #mkl_set_num_threads(1)
 
 def no_ax_fax(k=1,Gamma=1):
-  fig = figure(k,figsize=(6*Gamma,6))
-  ax  = Axes(fig,[0,0,1,1])
+  fig = pylab.figure(k,figsize=(6*Gamma,6))
+  ax  = pylab.Axes(fig,[0,0,1,1])
   ax.set_axis_off()
   fig.add_axes(ax)
   return fig,ax
